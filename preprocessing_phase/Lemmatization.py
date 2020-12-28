279940505e16d29a4e1aa775ac38bc5309750acb
vlavrent <73958916+vlavrent@users.noreply.github.com>
2020-12-28 18:30:07 +0200

    Lemmatization

--- preprocessing_phase/Lemmatization.py
+++ preprocessing_phase/Lemmatization.py
@@ -2,17 +2,19 @@ import spacy
 import pandas as pd
 from google.colab import drive
 
+#-------Mounting (connecting to)  drive------
 drive.mount('drive',force_remount=True)
 
-
+#-------Load Data and Remove Null values-------------------
 books = pd.read_csv(r"Processed-Books.csv",encoding="cp1253")
 data = books.dropna()
 
-nlp = spacy.load('el')
+#-----------Load el_core_news_sm from spacy----------
+nlp = spacy.load('el_core_news_sm')
 
-
+#------------Lemmatize words----------------------------
 data['Περιεχόμενα'] = data['Περιεχόμενα'].apply(lambda x: ' '.join([w.lemma_ for w in nlp(x)]))
 
-
+#-----------Save Lemmatized dataset to a CSV file on google drive-----------------------
 data.to_csv("Lemma.csv",index=False)
 !cp Lemma.csv "drive/My Drive/"
